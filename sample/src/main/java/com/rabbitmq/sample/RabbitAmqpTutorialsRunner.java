0f005df19780d74b6a9fd1ac11281c4f5e9bf81a
Araujo <daniel.araujo@oi.net.br>
2017-08-20 20:03:44 +0000

    Cleaning classes in order to separated producer and consumer.

--- sample/src/main/java/com/rabbitmq/sample/RabbitAmqpTutorialsRunner.java
+++ sample/src/main/java/com/rabbitmq/sample/RabbitAmqpTutorialsRunner.java
@@ -4,22 +4,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by daniel on 18/08/17.
  */
+@Configuration
 public class RabbitAmqpTutorialsRunner implements CommandLineRunner {
 
-    @Value("${tutorial.client.duration:0}")
-    private int duration;
+//    @Value("${tutorial.client.duration:0}")
+//    private int duration;
 
-    @Autowired
-    private ConfigurableApplicationContext ctx;
+//    @Autowired
+//    private ConfigurableApplicationContext ctx;
 
     @Override
     public void run(String... arg0) throws Exception {
-        System.out.println("Ready ... running for " + duration + "ms");
-        Thread.sleep(duration);
-        ctx.close();
+        while (true) {
+//            System.out.println("Ready ... running");
+        }
+//        System.out.println("Ready ... running for " + duration + "ms");
+//        Thread.sleep(duration);
+//        ctx.close();
     }
 }
--- sample/src/main/java/com/rabbitmq/sample/SampleApplication.java
+++ sample/src/main/java/com/rabbitmq/sample/SampleApplication.java
@@ -8,30 +8,30 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
+//@EnableScheduling
 public class SampleApplication {
 
-	@Profile("usage_message")
-	@Bean
-	public CommandLineRunner usage() {
-		return new CommandLineRunner() {
+//	@Profile("usage_message")
+//	@Bean
+//	public CommandLineRunner usage() {
+//		return new CommandLineRunner() {
+//
+//			@Override
+//			public void run(String... arg0) throws Exception {
+//				System.out.println("This app uses Spring Profiles" +
+//						" to control its behavior.\n");
+//						System.out.println("Sample usage:" +
+//								" java -jar rabbit-tutorials.jar " +
+//								"--spring.profiles.active=hello-world,sender");
+//			}
+//		};
+//	}
 
-			@Override
-			public void run(String... arg0) throws Exception {
-				System.out.println("This app uses Spring Profiles" +
-						" to control its behavior.\n");
-						System.out.println("Sample usage:" +
-								" java -jar rabbit-tutorials.jar " +
-								"--spring.profiles.active=hello-world,sender");
-			}
-		};
-	}
-
-	@Profile("!usage_message")
-	@Bean
-	public CommandLineRunner tutorial() {
-		return new RabbitAmqpTutorialsRunner();
-	}
+//	@Profile("!usage_message")
+//	@Bean
+//	public CommandLineRunner tutorial() {
+//		return new RabbitAmqpTutorialsRunner();
+//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SampleApplication.class, args);
