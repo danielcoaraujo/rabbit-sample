3d0b4ec9e0655c1634d6b376e3c3c2ea8cd2c398
rafael.araujo <rafael.araujo@bemobi.com.br>
2017-08-20 22:41:37 -0300

    Cleaning useless codes

--- consumer/src/main/java/com/rabbit/consumer/configuration/RabbitConfiguration.java
+++ consumer/src/main/java/com/rabbit/consumer/configuration/RabbitConfiguration.java
@@ -1,14 +1,7 @@
 package com.rabbit.consumer.configuration;
 
-import com.rabbit.consumer.model.QueueNames;
 import com.rabbit.consumer.model.Receiver;
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
-import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,36 +10,6 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class RabbitConfiguration {
-
-//    @Bean
-//    Queue queue() {
-//        return new Queue(QueueNames.TEST, false);
-//    }
-//
-//    @Bean
-//    TopicExchange exchange() {
-//        return new TopicExchange("spring-boot-exchange");
-//    }
-//
-//    @Bean
-//    Binding binding(Queue queue, TopicExchange exchange) {
-//        return BindingBuilder.bind(queue).to(exchange).with(QueueNames.TEST);
-//    }
-//
-//    @Bean
-//    SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
-//                                             MessageListenerAdapter listenerAdapter) {
-//        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
-//        container.setConnectionFactory(connectionFactory);
-//        container.setQueueNames(QueueNames.TEST);
-//        container.setMessageListener(listenerAdapter);
-//        return container;
-//    }
-//
-//    @Bean
-//    MessageListenerAdapter listenerAdapter(Receiver receiver) {
-//        return new MessageListenerAdapter(receiver, "receiveMessage");
-//    }
 
     @Bean
     public Queue hello() {
